0d191eb8815b962286362b18266bf59299e435a9
Bex Rakhim <bexultan.rakhim@nu.edu.kz>
2021-09-20 21:41:30 +0900

    "small fix of the packages"

--- common/test/test_model.py
+++ common/test/test_model.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("../jaxcontrol")
 from jaxcontrol.numeric.integrators import Euler
-from jaxcontrol.models.pendulum import Pendulum
+from jaxcontrol.models import Pendulum
 import jax.numpy as jnp
 ind = Euler()
 mod = Pendulum(ind,L=1.0)
