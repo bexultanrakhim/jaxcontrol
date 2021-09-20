896c4b0b868a731f9aa22cb9851083ca0e2bc6f6
Bex Rakhim <bexultan.rakhim@nu.edu.kz>
2021-09-20 22:39:50 +0900

    "more integrators"

--- jaxcontrol/numeric/integrators.py
+++ jaxcontrol/numeric/integrators.py
@@ -15,3 +15,17 @@ class Integrator(ABC):
 class Euler(Integrator):
     def integration(self, model, x, u):
         return x + self.dt*model(x,u)
+
+class MidPoint(Integrator):
+    def integration(self, model, x, u):
+        mid = self.model(x, u)
+        return x + self.dt*model(x+ mid*self.dt/2, u)
+
+
+class RK4(Integrator):
+    def integration(self, model, x, u):
+        k1 = model(x, u)
+        k2 = model(x + self.dt*k1/2, u)
+        k3 = model(x + self.dt*k2/2, u)
+        k4 = model(x + self.dt*k3, u)
+        return x + (self.dt/6)*(k1 + 2*k2 + 2*k3 + k4)
